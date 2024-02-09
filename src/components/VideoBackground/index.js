96b7c427d36b88e4c936c7840db52c75adf4c0d8
Josh Pascual <joshapascual@gmail.com>
2024-02-08 22:50:28 -0800

    fjdklsa

--- src/components/VideoBackground/index.js
+++ src/components/VideoBackground/index.js
@@ -1,18 +1,25 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Fragment } from "react";
+import { Transition } from "@headlessui/react";
 import space from '../../backgrounds/space.mp4'
 import forest from '../../backgrounds/forest.mp4'
 import ocean from '../../backgrounds/ocean.mp4'
 
 export default function VideoBackground({ background }) {
     const [theme, setTheme] = useState(space)
+    const [isVisible, setIsVisible] = useState(false)
 
     useEffect(() => {
         if (background === 'space') setTheme(space)
         if (background === 'forest') setTheme(forest)
         if (background === 'ocean') setTheme(ocean)
+
+        setIsVisible(false);
+        setTimeout(() => setIsVisible(true), 100);
     }, [background])
 
     return (
-        <video className='absolute top-0 object-cover h-full w-full -z-10' autoPlay controlsList="nodownload nofullscreen noremoteplayback" loop muted playsInline preload="auto" src={theme} type="video/mp4" />
+        <div className="absolute top-0 left-0 w-full h-full bg-black">
+            <video className={`absolute top-0 object-cover h-full w-full ${isVisible ? 'opacity-100 transition-opacity duration-500' : 'opacity-0'}`} autoPlay controlsList="nodownload nofullscreen noremoteplayback" loop muted playsInline preload="auto" src={theme} type="video/mp4" />
+        </div>
     )
 }
